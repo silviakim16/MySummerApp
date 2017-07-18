0a5982b9adbdf6db9a94fa6f1bd3ef0e03569163
Silvia  Kim <silviakim@Silvias-MacBook-Air.local>
2017-07-18 09:36:25 -0700

    Initial Commit

---
+++ SummerApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SummerApp
+//
+//  Created by Silvia  Kim on 7/18/17.
+//  Copyright © 2017 Silvia  Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
